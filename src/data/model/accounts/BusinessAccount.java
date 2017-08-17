217c64f26f0e89ac94ab28c023924988b1543658
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-08-17 16:39:03 +0200

    add acounts

---
+++ src/data/model/accounts/BusinessAccount.java
@@ -0,0 +1,12 @@
+package data.model.accounts;
+
+public class BusinessAccount extends Account {
+
+	public BusinessAccount(String accountType, int userID, double accountNumber, double money) {
+		super(accountType, userID, accountNumber, money);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
---
+++ src/util/AccountManage.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import data.holder.AccountsHolder;
+import data.model.accounts.Account;
+import data.model.accounts.BusinessAccount;
+import data.model.accounts.PrivateAccount;
+
+
+public class AccountManage {
+	
+	private static ArrayList<Account> account = AccountsHolder.getAccount();
+	
+	private static Scanner in = new Scanner(System.in);
+	
+	public static void addPrivateAccount(int userID) {
+		
+		System.out.println("How much money you pay to this account");
+		double money = in.nextDouble();
+		double accountNumber = 0;
+		
+		account.add(new PrivateAccount("PRIVATE", userID, accountNumber, money));
+		
+	}
+	
+	public static void addBusinesAccount(int userID) {
+		
+		System.out.println("How much money you pay to this account");
+		double money = in.nextDouble();
+		double accountNumber = 0;
+		
+		account.add(new BusinessAccount("BUSINESS", userID, accountNumber, money));
+		
+	}
+
+	
+	
+}
