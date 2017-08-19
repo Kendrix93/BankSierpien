48d16ed26f390650715858c09b749f3712489f40
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-08-19 16:52:41 +0200

    added read from file payments

--- src/util/ReadFromSCV.java
+++ src/util/ReadFromSCV.java
@@ -7,20 +7,23 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import data.holder.AccountsHolder;
+import data.holder.PaymentHolder;
 import data.holder.PeopleHolder;
 import data.model.accounts.Account;
 import data.model.accounts.BusinessAccount;
 import data.model.accounts.PrivateAccount;
+import data.model.payment.Payment;
 import data.model.people.User;
 
 public class ReadFromSCV {
 	
 	private static ArrayList<Account> account = AccountsHolder.getAccount();
 	private static ArrayList<User> user = PeopleHolder.getUser();
+	private static ArrayList<Payment> payment = PaymentHolder.getPayment();
 	
 	static String filename1 = "Users.csv";
 	static String filename2 = "Accounts.csv";
-	
+	static String filename3 = "Payments.csv";
 	
 	
 	public static ArrayList<User> readFromFileUser(){
@@ -30,7 +33,7 @@ public class ReadFromSCV {
 		try {
 			read = (ArrayList<String>) Files.readAllLines(path);
 		} catch (IOException ex) {
-			System.out.println("There is no file");
+			System.out.println("There is no file Users");
 		}
 		ArrayList<User> user = toObject(read);
 		return user;
@@ -56,7 +59,7 @@ public class ReadFromSCV {
 		try {
 			read = (ArrayList<String>) Files.readAllLines(path);
 		} catch (IOException ex) {
-			System.out.println("There is no file");
+			System.out.println("There is no file Accounts");
 		}
 		ArrayList<Account> account = toObject1(read);
 		return account;
@@ -79,6 +82,33 @@ public class ReadFromSCV {
 		}
 		return account;
 	}
+	
+	
+	public static ArrayList<Payment> readFromFilePayments(){
+		
+		Path path = Paths.get(filename3);
+		ArrayList<String> read = new ArrayList<String>();
+		try {
+			read = (ArrayList<String>) Files.readAllLines(path);
+		} catch (IOException ex) {
+			System.out.println("There is no file Payment");
+		}
+		ArrayList<Payment> payment = toObject2(read);
+		return payment;
+		
+	}
+	
+	public static ArrayList<Payment> toObject2(ArrayList<String> read) {
+		
+		for(String line : read) {
+			String [] l = line.split(";");
+			Payment payments = new Payment(Integer.parseInt(l[0]), Double.parseDouble(l[1]),
+					Double.parseDouble(l[2]), Double.parseDouble(l[3]), Integer.parseInt(l[4]));
+			payment.add(payments);
+		}
+		return payment;
+	}
+	
 	
 
 }
